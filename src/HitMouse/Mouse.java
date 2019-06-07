098929477468d4fc20870041ee6b31b6abe7df4c
LiaozhiCheng <51042415+LiaozhiCheng@users.noreply.github.com>
2019-06-07 17:40:12 +0800

    HitMouse修改
    加入點擊聲音
    刻好左邊的介面

--- src/HitMouse/Mouse.java
+++ src/HitMouse/Mouse.java
@@ -1,85 +1,170 @@
 package HitMouse;
-import House.house.House;
-import Main.Main;
+
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.Timer;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
+import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.util.Random;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 import HitMouse.gameobject.*;
 import HitMouse.gameobject.Character;
+
+import Main.*;
+import House.house.House;
 
 import java.lang.Thread;
 
 public class Mouse extends JLayeredPane{
     private JFrame frame;
-    private ImageIcon[] teacher=new ImageIcon[6];
-    private ImageIcon[] student = new ImageIcon[5];
-    private ImageIcon kaishi;
-    private Main mainFrame ;
+
+
+
+    private ImageIcon imageBackGround;//背景圖
+    private ImageIcon[] teacher=new ImageIcon[6];//老師圖片(好地鼠)
+    private ImageIcon[] student = new ImageIcon[5];//學生圖片(壞地鼠)
+    private ImageIcon startImage;//開始圖片
+    private ImageIcon backToMainImage;
+    private ImageIcon disCountImage;
+    private ImageIcon expandMoneyImage;
+    private ImageIcon ruleImage;
+    private ImageIcon testCharacterImage;
+
+    private JButton btnStart;//開始按鈕
+    private JButton  btnItemOnlyTeacher;//使用老師道具的按鈕
+    private JButton btnItemScoreDouble;
+    private JButton[] win;//放窗戶的圖片
+    private JButton[] but;//人物出現的位置
+
+    private Timer itemTeacherTime;//使用道具 生效時間
+    private Timer itemScoreDouble;
+
+    private int specialCard;//紀錄道具使用與否
+    private final int SHIFT=300;
+    private Score score;
+
+    private JLabel backGroundLabel;
+    private JLabel backToMainLabel;
+    private JLabel disCountLabel;
+    private JLabel expandMoneyLabel;
+    private JLabel ruleLabel;
+    private JLabel testCharacterLabel;
+    private JTextField scoreText;
+    private Main mainFrame;
     private House house;
-    public Mouse(Main mainFrame,House house){
+     public Mouse(Main mainFrame,House house){
+    //public Mouse(){
         this.house=house;
         this.mainFrame=mainFrame;
-        ImageIcon image=new ImageIcon("data/HitMouse/background/background.png");//background
+
        /* frame = new JFrame("打地鼠");
         frame.setSize(900,675);
         //frame.setBounds(0,0,900,675);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-
-        JLayeredPane layeredPane=new JLayeredPane();
-
-        JPanel panel=new JPanel();
-        frame.add(panel);
-        panel.setLayout(null);
-        panel.setBounds(0,0,image.getIconWidth(),image.getIconHeight());
-        panel.add(layeredPane);
 */
-       // layeredPane.add(panel,JLayeredPane.DEFAULT_LAYER);  //背景放在最下層
-        JLabel jl=new JLabel(image);
-        jl.setBounds(0,0,image.getIconWidth(),image.getIconHeight());
-        add(jl,JLayeredPane.DEFAULT_LAYER);
+       imageBackGround=new ImageIcon("data/HitMouse/background/background.png");//background
+        backGroundLabel=new JLabel(imageBackGround);
+        backGroundLabel.setBounds(SHIFT,0,imageBackGround.getIconWidth(),imageBackGround.getIconHeight());
+        add(backGroundLabel,JLayeredPane.DEFAULT_LAYER);
 
-        // panel.add(jl);
-        //frame.add(layeredPane);
-        /*
-        frame.setLayeredPane(layeredPane);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        */
+
+        backToMainImage = new ImageIcon("data/HitMouse/icon/backhome.png");
+        backToMainLabel = new JLabel(backToMainImage);
+        backToMainLabel.setBounds(0,0,backToMainImage.getIconWidth(),backToMainImage.getIconHeight());
+        add(backToMainLabel,JLayeredPane.DEFAULT_LAYER);
+
+        disCountImage = new ImageIcon("data/HitMouse/icon/discount.png");
+        disCountLabel = new JLabel(disCountImage);
+        disCountLabel.setBounds(0,backToMainImage.getIconHeight(),disCountImage.getIconWidth(),disCountImage.getIconHeight());
+        add(disCountLabel,JLayeredPane.DEFAULT_LAYER);
+
+        ruleImage = new ImageIcon("data/HitMouse/icon/rule.png");
+        ruleLabel = new JLabel(ruleImage);
+        ruleLabel.setBounds(0,backToMainImage.getIconHeight()+disCountImage.getIconHeight(),ruleImage.getIconWidth(),ruleImage.getIconHeight());
+        add(ruleLabel,JLayeredPane.DEFAULT_LAYER);
+
+        expandMoneyImage = new ImageIcon("data/HitMouse/icon/expandMoney.png");
+        expandMoneyLabel = new JLabel(expandMoneyImage);
+        expandMoneyLabel.setBounds(0,backToMainImage.getIconHeight()+disCountImage.getIconHeight()+ruleImage.getIconHeight(),expandMoneyImage.getIconWidth(),expandMoneyImage.getIconHeight());
+        add(expandMoneyLabel,JLayeredPane.DEFAULT_LAYER);
+
+
+        System.out.println(ruleImage.getIconHeight());
+        testCharacterImage = new ImageIcon("data/HitMouse/icon/test_charater.png");
+        testCharacterLabel = new JLabel(testCharacterImage);
+        testCharacterLabel.setBounds(0,397,testCharacterImage.getIconWidth(),testCharacterImage.getIconHeight());
+        add(testCharacterLabel,JLayeredPane.DEFAULT_LAYER);
+
+        scoreText = new JTextField();
+        scoreText.setEditable(false);
+        scoreText.setColumns(5);
+        scoreText.setBounds(175,115,30,20);
+
+
+        //add(scoreText,JLayeredPane.MODAL_LAYER);
+
         for(int i=0;i<6;i++){
             teacher[i]=new ImageIcon(Character.teachers[i]);
         }
         for(int i=0;i<5;i++){
             student[i]=new ImageIcon(Character.students[i]);
         }
-        kaishi=new ImageIcon("data/HitMouse/image/start.jpg");
-        JButton btnStart = new JButton();
-        btnStart.setIcon(kaishi);
-        btnStart.setBounds(736, 35, 125, 59);
+        startImage=new ImageIcon("data/HitMouse/image/start.jpg");
+        btnStart = new JButton();
+        btnStart.setIcon(startImage);
+        btnStart.setBounds(736+SHIFT, 35, 125, 59);
         add(btnStart,JLayeredPane.MODAL_LAYER);
-        JButton[] win=new JButton[16];
-        JButton[] but=new JButton[16];
+        btnItemOnlyTeacher = new JButton("道具");
+        btnItemOnlyTeacher.setBounds(200,580,100,30);
+        add(btnItemOnlyTeacher,JLayeredPane.MODAL_LAYER);
+        btnItemScoreDouble = new JButton("道具2");
+        btnItemScoreDouble.setBounds(200,610,100,30);
+        add(btnItemScoreDouble,JLayeredPane.MODAL_LAYER);
+
+        btnItemScoreDouble.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                score.setAddScore(score.getAddScore()*2);
+                score.setMinusScore(0);
+                itemScoreDouble.start();
+            }
+        });
+        itemScoreDouble = new Timer(10000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                score.setAddScore(score.getAddScore()/2);
+                score.setMinusScore(1);
+            }
+        });
+        itemTeacherTime = new Timer(10000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                specialCard=0;
+            }
+        });
+        btnItemOnlyTeacher.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                specialCard=1;
+                itemTeacherTime.start();
+
+
+            }
+        });
+        win=new JButton[16];
+        but=new JButton[16];
         int k=0;
         for(int i=65;i<=695;i+=210){
             for(int j=155;j<=505;j+=170){
 
                 but[k] = new JButton();
                // but[k].setIcon(new ImageIcon("data/HitMouse/background/窗戶.png"));
-                but[k].setBounds(i+2,j+2,150,130);
+                but[k].setBounds(i+2+SHIFT,j+2,150,130);
 
                 but[k].setBorderPainted(false);
                 but[k].setBorder(null);
@@ -91,7 +176,7 @@ public class Mouse extends JLayeredPane{
 
                 win[k] = new JButton();
                 win[k].setIcon(new ImageIcon("data/HitMouse/background/窗戶.png"));
-                win[k].setBounds(i,j,153,134);
+                win[k].setBounds(i+SHIFT,j,153,134);
                 win[k].setBorderPainted(false);
                 win[k].setBorder(null);
                 win[k].setFocusPainted(false);
@@ -100,24 +185,26 @@ public class Mouse extends JLayeredPane{
                 k++;
             }
         }
-        Score score =new Score();
-        JLabel djs =new JLabel("倒計時："); //Countdown
-        djs.setBounds(8, 16, 250, 59);
+        score =new Score();
+        JLabel djs =new JLabel(""); //Countdown
+        djs.setFont(new Font("標楷體", Font.BOLD, 25));;
+        djs.setBounds(175,115,30,25);
         add(djs,JLayeredPane.MODAL_LAYER);
 
 
         JLabel jfb =new JLabel("得分：0"); //Scoreboard
-        jfb.setBounds(8, 40, 200, 59);
+        jfb.setBounds(8+SHIFT, 40, 200, 59);
         add(jfb,JLayeredPane.MODAL_LAYER);
 
-
+        //開始按鈕並設定第一支地鼠出現的時間延遲
         Timer timer=new Timer(score.getSd()-10,new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
                 but[score.getLast()].setIcon(null);
                 score.setLast((int)(12*Math.random()));
                 int choose=(int) (2 * Math.random());
-                if(choose==0)  but[score.getLast()].setIcon(teacher[(int) (6 * Math.random())]);
+
+                if( choose==0 || specialCard==1)  but[score.getLast()].setIcon(teacher[(int) (6 * Math.random())]);
                 else but[score.getLast()].setIcon(student[(int) (5 * Math.random())]);
 
             }
@@ -126,9 +213,9 @@ public class Mouse extends JLayeredPane{
         //timer2 生成最後的得分訊息
         Timer time2=new Timer(2,new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                timer.setDelay(score.getSd());
+                timer.setDelay(score.getSd());//設定地鼠出現速度
                 jfb.setText("得分："+score.getScore());
-                djs.setText("倒計時："+(score.getStarttime()/1000+score.getDjs()-System.currentTimeMillis()/1000)+"s");
+                djs.setText((score.getStarttime()/1000+score.getDjs()-System.currentTimeMillis()/1000)+"");
                 if((score.getStarttime()/1000+score.getDjs()-System.currentTimeMillis()/1000)==0)
                 {
                     timer.stop();
@@ -146,7 +233,8 @@ public class Mouse extends JLayeredPane{
                 if((score.getStarttime()/1000+score.getDjs()-System.currentTimeMillis()/1000)==-1)
                 {
                     time2.stop();
-                    djs.setText("倒計時：");
+                    itemTeacherTime.stop();
+                    djs.setText("");
                 }
             }
         });
@@ -160,32 +248,34 @@ public class Mouse extends JLayeredPane{
         });
         for(int i=0;i<12;i++){
             final int ii=i;
-        //    Random  r = new Random();
             but[i].addMouseListener(new MouseListener() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
+
                     int c = e.getButton();// 得到按下的滑鼠鍵
                     if (c == MouseEvent.BUTTON1){ // 判斷是滑鼠左鍵按下
                         if (but[ii].getIcon() != null) {
+                            Sound.KissSound();
                             boolean judge=false;
-                            for(int j=0;j<6;j++){
+                            for(int j=0;j<6;j++){//圖片是老師
                                 if(Character.teachers[j].equals(but[ii].getIcon().toString())) judge=true;
                             }
-                            if(judge==true) score.setScore(score.getScore() + 1);
-                            else score.setScore(score.getScore() - 1);
+                            if(judge==true) score.setScore(score.getScore() + score.getAddScore());
+                            else score.setScore(score.getScore() - score.getMinusScore());
 
                             but[score.getLast()].setIcon(null);
-
+                            //Sound.KissSound();
                         }
                     }
                     if (c == MouseEvent.BUTTON3) {// 判斷是滑鼠右鍵按下
                         if (but[ii].getIcon() != null) {
+                            Sound.HitSound();
                             boolean judge=false;
                             for(int j=0;j<5;j++){
                                 if(Character.students[j].equals(but[ii].getIcon().toString())) judge=true;
                             }
-                            if(judge==true) score.setScore(score.getScore() + 1);
-                            else score.setScore(score.getScore() - 1);
+                            if(judge==true) score.setScore(score.getScore() + score.getAddScore());
+                            else score.setScore(score.getScore() - score.getMinusScore());
 
                             but[score.getLast()].setIcon(null);
 
@@ -194,12 +284,12 @@ public class Mouse extends JLayeredPane{
                 }
                 @Override
                 public void mousePressed(MouseEvent e) {
-
+                    mouseClicked(e);
                 }
 
                 @Override
                 public void mouseReleased(MouseEvent e) {
-
+                    mouseClicked(e);
                 }
 
                 @Override
---
+++ src/HitMouse/gameobject/Sound.java
@@ -0,0 +1,38 @@
+package HitMouse.gameobject;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+public class Sound {
+    private static Clip hitSound;
+    private static Clip kissSound;
+    public static void HitSound(){
+        try {
+            File soundFile = new File("data/music/hit.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            hitSound = AudioSystem.getClip();
+            hitSound.open(audioIn);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        hitSound.start();
+    }
+    public static void KissSound(){
+        try {
+            File soundFile = new File("data/music/kiss.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            kissSound = AudioSystem.getClip();
+            kissSound.open(audioIn);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+        kissSound.start();
+    }
+}
