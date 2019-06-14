ba2d69b3a0e801f166dbe3b6cd54c622017c9037
kazuyahooo <46344542+kazuyahooo@users.noreply.github.com>
2019-06-14 13:43:28 +0800

    新增圖片 結算(未完成

--- src/Main/GameSettlement.java
+++ src/Main/GameSettlement.java
@@ -9,36 +9,86 @@ import java.awt.event.MouseEvent;
 public class GameSettlement extends JPanel{
     private Main mainFrame;
     private House house;
-
+    private ImageIcon pair=new ImageIcon("data//main/pair.png");
+    private ImageIcon EXP=new ImageIcon("data//main/gameexp.png");
+    private ImageIcon gamePlay=new ImageIcon("data//main/gameplay.png");
+    private ImageIcon money=new ImageIcon("data//main/一袋錢.png");
+    private ImageIcon death=new ImageIcon("data//main/skullicon.png");
     public static void main(String[] Args){
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1200, 675);
 
     }
-    //public GameSettlement(Main mainFrame,House house){
     public GameSettlement(Main mainFrame,House house){
         this.mainFrame=mainFrame;
         this.house=house;
-        setLayout(new GridLayout(10,1));
-        JLabel label = new JLabel("遊戲結算");
-        JLabel label2 = new JLabel("消滅一對是一對");
-        JLabel label3 = new JLabel("得到總經驗值:");
-        JLabel label4 = new JLabel("得到總金錢:");
-        JLabel label5 = new JLabel("總遊玩次數:");
-        JLabel label6 = new JLabel("總找到對數:");
-        setLabel(label);
-        setLabel(label2);
-        setLabel(label3);
-        setLabel(label4);
-        setLabel(label5);
-        setLabel(label6);
-        add(label);
-        add(label2);
-        add(label3);
-        add(label4);
-        add(label5);
-        add(label6);
+        setLayout(null);
+        JLabel labelC2 = new JLabel(String.format(""+house.getTotalExp(1)));
+        JLabel labelC3 = new JLabel(String.format(""+house.getTotalMoney(1)));
+        JLabel labelC4 = new JLabel(String.format(""+house.getTotalPlay(1)));
+        JLabel labelC5 = new JLabel(String.format(""+house.getTotalMistake(1)));
+
+        JLabel labelH2 = new JLabel(String.format(""+house.getTotalExp(2)));
+        JLabel labelH3 = new JLabel(String.format(""+house.getTotalMoney(2)));
+        JLabel labelH4 = new JLabel(String.format(""+house.getTotalPlay(2)));
+        JLabel labelH5 = new JLabel(String.format(""+house.getTotalMistake(2)));
+
+        JLabel labelF2 = new JLabel(String.format(""+house.getTotalExp(3)));
+        JLabel labelF3 = new JLabel(String.format(""+house.getTotalMoney(3)));
+        JLabel labelF4 = new JLabel(String.format(""+house.getTotalPlay(3)));
+        JLabel labelF5 = new JLabel(String.format(""+house.getTotalMistake(3)));
+
+        JLabel labelD2 = new JLabel(String.format(""+house.getTotalExp(4)));
+        JLabel labelD3 = new JLabel(String.format(""+house.getTotalMoney(4)));
+        JLabel labelD4 = new JLabel(String.format(""+house.getTotalPlay(4)));
+        JLabel labelD5 = new JLabel(String.format(""+house.getTotalMistake(4)));
+        setLabel(labelC2);
+        setLabel(labelC3);
+        setLabel(labelC4);
+        setLabel(labelC5);
+        setLabel(labelH2);
+        setLabel(labelH3);
+        setLabel(labelH4);
+        setLabel(labelH5);
+        setLabel(labelF2);
+        setLabel(labelF3);
+        setLabel(labelF4);
+        setLabel(labelF5);
+        setLabel(labelD2);
+        setLabel(labelD3);
+        setLabel(labelD4);
+        setLabel(labelD5);
+
+        labelC2.setBounds(101,167,174,59);
+
+        add(labelC2);
+        /*add(labelC3);
+        add(labelC4);
+        add(labelC5);
+        add(labelH2);
+        add(labelH3);
+        add(labelH4);
+        add(labelH5);
+        add(labelF2);
+        add(labelF3);
+        add(labelF4);
+        add(labelF5);
+        add(labelD2);
+        add(labelD3);
+        add(labelD4);
+        add(labelD5);*/
+
+        JButton back =new JButton("返回按鈕");
+        //add(back,BorderLayout.SOUTH);
+
+        ImageIcon img = new ImageIcon("data/main/byebyebyebye.png");
+        Image i = img.getImage();
+        i = i.getScaledInstance(1200, 675, Image.SCALE_SMOOTH);
+        JLabel background = new JLabel();
+        background.setIcon(new ImageIcon(i));
+        background.setSize(1200, 675);
+        this.add(background);
     }
 
     //設計道具按鈕
